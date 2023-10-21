33ef47fcb2ea02ee5a73a0ad8db797617cf92e58
Diogo Brasil <diogobrasildasilva@gmail.com>
2023-10-21 19:58:00 -0300

    Push do código da minha maquina para o gitHub.

---
+++ src/Conversor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Conversor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Número para conversão para binário:");
+        int numero = input.nextInt();
+        String binario = "";
+
+        while (numero != 0) {
+            int resto = numero % 2;
+            binario = resto + binario;
+            numero /= 2;
+        }
+
+        System.out.println("Em binário: " + binario);
+
+        input.close();
+    }
+}
